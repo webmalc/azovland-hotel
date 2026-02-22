ea1263969265fb8531ab3a9277e4a9ba89167508
webmalc <m@webmalc.pw>
2026-02-23 00:26:26 +0300

    Add logging

--- az/settings/base.py
+++ az/settings/base.py
@@ -248,42 +248,21 @@ LOGGING = {
     "disable_existing_loggers": False,
     "formatters": {
         "verbose": {
-            "format": ("[%(asctime)s] %(levelname)s [%(name)s:%(lineno)s] %(message)s"),
+            "format": "[%(asctime)s] %(levelname)s [%(name)s:%(lineno)s] %(message)s",
             "datefmt": "%d/%b/%Y %H:%M:%S",
         },
-        "simple": {"format": "%(levelname)s %(message)s"},
-    },
-    "filters": {
-        "require_debug_false": {
-            "()": "django.utils.log.RequireDebugFalse",
-        }
     },
     "handlers": {
-        "file": {
-            "level": "DEBUG",
-            "class": "logging.FileHandler",
-            "filename": "logs/app.log",
-            "formatter": "verbose",
-        },
-        "error_file": {
-            "level": "ERROR",
-            "class": "logging.FileHandler",
-            "filename": "logs/error.log",
+        "console": {
+            "class": "logging.StreamHandler",
+            "stream": "ext://sys.stdout",
             "formatter": "verbose",
         },
     },
     "loggers": {
         "app": {
-            "handlers": ["file", "error_file"],
+            "handlers": ["console"],
             "level": "DEBUG",
         },
-        # "django": {
-        #     "handlers": ["error_file"],
-        #     "level": "ERROR",
-        # },
-        # "wagtail": {
-        #     "handlers": ["error_file"],
-        #     "level": "ERROR",
-        # },
     },
 }
